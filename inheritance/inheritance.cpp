13028126763882c562cac3ea761ad5ebaa8245bf
admin <admin@DESKTOP-D6KE8CP>
2022-11-21 17:03:10 +0400

    Add project files.

---
+++ inheritance/inheritance.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+using namespace std;
+
+//Single inheritance
+class Animal {
+
+};
+
+class Cat : Animal {
+
+};
+
+
+//Multi level inheritance
+class Animal {
+
+};
+
+class Cat : Animal {
+
+};
+
+class Kitty : Cat {
+
+};
+
+
+//Multiple inheritance
+class Dad {
+
+};
+
+class Mom {
+
+};
+
+class child : Dad, Mom {
+
+};
+
+
+//Hierarchy
+class Mom {
+
+};
+
+class daughter : Mom{
+
+};
+
+class son : Mom{
+
+};
+
+void main() {
+
+}
